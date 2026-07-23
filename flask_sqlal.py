0d6f9bfe10fc6973dc77193498a4f145214eb333
Collins Kibet <kollcibe05@gmail.com>
2026-07-23 17:59:53 +0300

    using random choice, app context, faker

---
+++ flask_sqlal.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
+
+class Pet(db.Model):
+    __tablename__ = "pets"
+
+    id =  db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, index=True)
+    species = db.Column(db.String, nullable=False)
+    # verified = db.Column(db.Boolean, default=False) # for demo on constraints
+ 
+

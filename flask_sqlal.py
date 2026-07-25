cc76504eb0d5f13b54a852b7689bb7706b75b7a0
Collins Kibet <kollcibe05@gmail.com>
2026-07-25 10:01:14 +0300

    flask models using joint flask_sqlal and sqlal metadata, SerializerMixin

--- flask_sqlal.py
+++ flask_sqlal.py
@@ -1,16 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+from sqlalchemy_serializer import SerializerMixin  # Serialization eg. to_dict()
 
 
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-class Pet(db.Model):
+class Pet(db.Model, SerializerMixin):
     __tablename__ = "pets"
 
     id =  db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, index=True)
     species = db.Column(db.String, nullable=False)
     # verified = db.Column(db.Boolean, default=False) # for demo on constraints
+    def __repr__(self):
+        return f"<Pet {self.id}, {self.name}, {self.species}>" # 

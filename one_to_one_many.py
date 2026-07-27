872ebc75f396b51c493669f5f1e7effe6e557222
Collins Kibet <kollcibe05@gmail.com>
2026-07-27 08:11:08 +0300

    naming_convention, cascade, uselist

--- one_to_one_many.py
+++ one_to_one_many.py
@@ -36,31 +36,46 @@ covention = {
 db = SQLAlchemy(metadata=metadata)
 
 class Employee(db.Model, SerializerMixin):
-    ___tablename__ = "employees"
+    __tablename__ = "employees"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    reviews = db.relationship("Review", back_populates='employee', cascade="all, delete-orphan")
+    onboarding = db.relationship(
+        "Onboarding", uselist=False, back_populates="employee", cascade='all, delete-orphan'
+    )
+
+
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
 class Onboarding(db.Model, SerializerMixin):
-    ___tablename__ = "onboardings"
+    __tablename__ = "onboardings"
 
     id = db.Column(db.Integer, primary_key=True)
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
+
+    employee = db.relationship("Employee", back_populates="onboarding")
+
+
+
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
 class Review(db.Model, SerializerMixin):
-    ___tablename__ = "reviews"
+    __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
+
+    employee = db.relationship("Employee", back_populates="reviews")
 
     def __repr__(self):
          return f'<Review {self.id}, {self.year}, {self.summary}>'

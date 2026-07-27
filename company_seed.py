38112e219f8da902068c61f6864fc02aa4d06f73
Collins Kibet <kollcibe05@gmail.com>
2026-07-27 08:08:56 +0300

    seeding using datetime, object ref.

--- company_seed.py
+++ company_seed.py
@@ -15,15 +15,15 @@ with app.app_context():
     db.session.add_all([uri, tristan])
     db.session.commit()
 
-    uri_2023 = Review(year=2023, summary="Great web developer!")
-    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work")
-    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches")
-    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker")
+    uri_2023 = Review(year=2023, summary="Great web developer!", employee_id=1)
+    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work", employee=tristan) # We use the object reference rather than integer after establishing the `relationship` using back_populates
+    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches", employee=tristan)
+    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker", employee=tristan)
     db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
     db.session.commit()
 
-    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27))
-    tristan_onboarding = Onboarding(orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True)
+    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27), employee=uri) # Of course, object ref only after establishing relationship.
+    tristan_onboarding = Onboarding(orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True, employee=tristan)
 
     db.session.add_all([uri_onboarding, tristan_onboarding])
     db.session.commit()

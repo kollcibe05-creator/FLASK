e30845c4a9ccdf9dfa7d2428727741b04619a0d3
Collins Kibet <kollcibe05@gmail.com>
2026-08-03 18:47:12 +0300

    hybrid_property, bcrypt, werkzeug.security, simple_hash with bytearray

--- iam_password_protection_model.py
+++ iam_password_protection_model.py
@@ -1,20 +1,83 @@
-from iam_password_protection_model import User
+from sqlalchemy.ext.hybrid import hybrid_property
 
-class Login(Resource):
-    def post(self):
-        username = request.get_json()['username']
-        user = User.query.filter(User.username == username)
-
-        password = request.get_json()['password']
-
-        if user.authenticate(password): # authenticate() defined as instance method
-            session['user_id'] = user.id
-            return user.to_dict(), 200
-        return {'error': 'Invalid username or password'}, 401
+def simple_hash(input):
+    return sum(bytearray(input, encoding='utf-8'))
 
 
-# Using Bcrypt
-from flask.ext.bcrypt import Bcrypt
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
 
-bcrypt = Bcrypt(app)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+
+    _password_hash = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.username}, ID {self.id}'
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = self.simple_hash(password)
+
+    def authenticate(self, password):
+        return self.simple_hash(password) == self.password_hash # simple hash returns an integer
+    # password_hash is 
+    @staticmethod
+    def simple_hash(input):
+        # return sum(bytearray(input, encoding='utf-8'))
+        return str(sum(bytearray(input, encoding='utf-8'))) # to avoid mismatch when matching with simple_hash
+
+        
+
+
+# werkzeug can be used to generate password hashes
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@hybrid_property
+def password_hash(self):
+    return self._password_hash
+
+@password_hash.setter
+def password_hash(self, password):
+    # Hashes the plain text password securely with salt
+        self._password_hash = generate_password_hash(password)
+
+def authenticate(self, password):
+    # Safely checks plain text password against stored secure hash
+    return check_password_hash(self.password_hash, password)
+
+
+# Using Bycrypt
+
+from app import bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.username}, ID {self.id}'
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
 

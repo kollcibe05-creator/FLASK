95efd3494fb888b3e6cd091be710d49eba23c7d4
Collins Kibet <kollcibe05@gmail.com>
2026-08-06 21:42:19 +0300

    str methods

--- index.py
+++ index.py
@@ -5,4 +5,10 @@ record_dict = {}
 for name in list_:
     record_dict[name] = record_dict.get(name, 0) + 1
 
-print(record_dict)
+# print(record_dict)
+
+print('name is collo'.title())
+print('name is collo'.upper())
+print('name is collo'.capitalize())
+
+print('name'.startswith('n'))

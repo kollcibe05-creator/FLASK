b17163faaef3b845207eeec8a52f5f96559acfec
Collins Kibet <kollcibe05@gmail.com>
2026-08-14 06:43:32 +0300

    app configuration recap

--- index.py
+++ index.py
@@ -20,3 +20,31 @@ fake = Faker()
 
 print(fake.catch_phrase())
 print(fake.sentence())
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData(
+    naming_convention = {}
+)
+db = SQLAlchemy(metadata=metadata)
+
+class Magazine(db.metadata):
+    def __init__(self, name):
+        self.name = name
+
+from flask_restful import Api, Resource
+
+from flask import Flask, make_response
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///magazines.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+bcrypt = Bcrypt(app)
